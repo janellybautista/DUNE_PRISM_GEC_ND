7ca99244396bb055c921a0adac11a6ac2419d87f
FlynnGuo <93301049+FlynnYGUO@users.noreply.github.com>
2023-11-02 21:48:58 -0400

    Run ND eff job

--- Bashfiles/Slurm_nnhome_ND.sh
+++ Bashfiles/Slurm_nnhome_ND.sh
@@ -6,9 +6,9 @@
 #SBATCH --mail-user=Flynn.Y.Guo@stonybrook.edu
 #SBATCH --nodelist=cedar # --nodes=4 --gres=gpu
 #SBATCH -c 30
-#SBATCH --time=96:00:00
+#SBATCH --time=180:00:00
 #!/bin/bash
 
 cd /home/fyguo/testbaroncode
-python3 ~/NeutrinoPhysics/GEC/BaronNewCode/code/new_hadron_muon_mktree.py $1
+python3 ~/home/fyguo/DUNE_PRISM_GEC_ND/code/new_hadron_muon_mktree.py $1
 wait
